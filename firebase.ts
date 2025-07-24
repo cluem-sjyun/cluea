5e9c16e0f9e719fac4509c5e5bec4c5d84b68554
cluem-sjyun <sjyun@cluem.com>
2025-07-24 15:11:09 +0900

    delete: firebase throw

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,3 @@
-// firebase.ts
-"use client"; // 명시적으로 클라이언트 전용
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -13,11 +10,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
-
-// 반드시 클라이언트에서만 초기화
-if (typeof window === "undefined") {
-  throw new Error("Firebase는 클라이언트에서만 사용하세요!");
-}
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
